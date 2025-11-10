baf77f8c2d7efa02bda49365cbe2a12157d26b39
unknown <saniyakhan7019@gmail.com>
2025-11-10 16:25:32 +0530

    changes

--- Backend/src/controller/authcontroller.js
+++ Backend/src/controller/authcontroller.js
@@ -50,9 +50,9 @@ async function login(req,res) {
     return res.status(200).json({
         message:"user logged in succesfully"
     })
-  }catch(err){
+  }catch(error){
     return res.status(400).json({
-        message:`err${err}`
+        message:`err${error}`
     })
   }
 }
